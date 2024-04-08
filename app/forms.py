a0ed804f84bfd3d31fa20181fca36826bf49efb3
Amelia Pellington <pellingtonamelia@gmail.com>
2024-04-07 20:51:30 -0700

    'lab5'

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,14 @@
 # Add any form classes for Flask-WTF here
+from django import forms
+
+class MovieForm(forms.Form):
+    title = forms.CharField(max_length=100)
+    description = forms.CharField(widget=forms.Textarea)
+    poster = forms.ImageField()
+
+    def clean_poster(self):
+        poster = self.cleaned_data.get('poster')
+        if poster:
+            if not poster.content_type.startswith('image'):
+                raise forms.ValidationError("Only image files are allowed for the movie poster.")
+        return poster
